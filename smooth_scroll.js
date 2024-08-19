f0017d38689ac26053a48a903b1b8e47c1226195
tommytrg <tomastrg22@gmail.com>
2024-08-19 14:04:05 +0200

    feat: remove scroll and update topbars

--- smooth_scroll.js
+++ smooth_scroll.js
@@ -1,14 +1,14 @@
-Object.values(document.getElementsByTagName("a"))
-  .filter(
-    (anchor) =>
-      anchor.getAttribute("href") && anchor.getAttribute("href").includes("#")
-  )
-  .forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault()
+// Object.values(document.getElementsByTagName("a"))
+//   .filter(
+//     (anchor) =>
+//       anchor.getAttribute("href") && anchor.getAttribute("href").includes("#")
+//   )
+//   .forEach((anchor) => {
+//     anchor.addEventListener("click", function (e) {
+//       e.preventDefault()
 
-      document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth",
-      })
-    })
-  })
+//       document.querySelector(this.getAttribute("href")).scrollIntoView({
+//         behavior: "smooth",
+//       })
+//     })
+//   })
